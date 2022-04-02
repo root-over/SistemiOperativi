2321b031050b24bcc2e63a6ff79918f499d3f018
root-over <magliano_giuseppe@virgilio.it>
2022-04-02 17:28:04 +0200

    finito classe ProdottoScalare

--- codiceSO/PrimaEsercitazione/Esercizi/ProdottoScalare.java
+++ codiceSO/PrimaEsercitazione/Esercizi/ProdottoScalare.java
@@ -10,10 +10,20 @@ public class ProdottoScalare extends Thread{
         this.a = Arrays.copyOf(a, a.length);
         this.b = Arrays.copyOf(b, b.length);
     }
-
+    @Override
     public void run(){
         for (int i=0; i<a.length; i++){
             tot += a[i]*b[i];
         }
+    }
+
+    public static void main(String[] args) {
+        int []a = new int[10];
+        int []b = new int [10];
+
+        Thread t = new Thread("Thread 1");
+        t.start();
+
+
     }
 }
