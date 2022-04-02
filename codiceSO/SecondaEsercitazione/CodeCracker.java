134175ba6cafd54b35fdcf3f54f330eb1c59622c
root-over <magliano_giuseppe@virgilio.it>
2022-04-02 21:18:20 +0200

    Staiu nescendu pacciu

--- codiceSO/SecondaEsercitazione/CodeCracker.java
+++ codiceSO/SecondaEsercitazione/CodeCracker.java
@@ -1,27 +1,61 @@
 package SecondaEsercitazione;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Random;
+import java.lang.*;
 
 
 public class CodeCracker implements Runnable{
-    StringBuilder codice= new StringBuilder();
     ArrayList<Integer> ar = new ArrayList<>();
 
     @Override
     public void run() {
-        Random r = new Random();
-        int len =0;
-        int num = r.nextInt(Integer.MAX_VALUE);
-        if (ar.contains(num)) {
-            Thread.currentThread().interrupt();
+        while (ar.size()<Integer.MAX_VALUE) {
+            Random r = new Random();
+            StringBuilder codice = new StringBuilder();
+            int len = 0;
+            int num = r.nextInt(Integer.MAX_VALUE);
+            if (ar.contains(num)) {
+                Thread.currentThread().interrupt();
+            }
+            ar.add(num);
+            len = 16 - String.valueOf(num).length();
+            for (int i = 0; i < len; i++) {
+                codice.append("0");
+            }
+            codice.append(String.valueOf(num));
+            try {
+                decripta(codice.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        ar.add(num);
-        len = 16-String.valueOf(num).length();
-        for (int i=0; i<len; i++) {
-            codice.append("0");
+    }
+
+    private void decripta(String codice) throws Exception {
+        String key = codice;
+        File inputFile = new File("document.txt");
+        File encryptedFile = new File("/home/root_over/Documents/Universita/Anno 2/Sistemi operativi/Eserciztazioni/codiceSO/SecondaEsercitazione/document2022.encrypted");
+        File decryptedFile = new File("/home/root_over/Documents/Universita/Anno 2/Sistemi operativi/Eserciztazioni/codiceSO/SecondaEsercitazione/prove/document2022.decrypted");
+
+        try{
+            CryptoUtils.decrypt(key,encryptedFile,decryptedFile);
+        }catch (CryptoException ex){
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
         }
-        codice.append(String.valueOf(num));
-        //TODO PROVA IL CODICE PER DECRIPTARE IL FILE
     }
 
     public static void main(String[] args) {
---
+++ codiceSO/SecondaEsercitazione/CryptoException.java
@@ -0,0 +1,10 @@
+package SecondaEsercitazione;
+
+public class CryptoException extends Exception {
+    public CryptoException() {
+    }
+
+    public CryptoException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
