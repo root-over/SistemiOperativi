16c868456da984850343b82a96e9452393512f5e
root-over <magliano_giuseppe@virgilio.it>
2022-04-08 19:24:12 +0200

    Ho sistemato un po di robe e implementato tutto in una classe

--- codiceSO/SecondaEsercitazione/CodeCracker.java
+++ codiceSO/SecondaEsercitazione/CodeCracker.java
@@ -4,31 +4,39 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.io.*;
+import java.lang.*;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Random;
-import java.lang.*;
 
 
 public class CodeCracker extends Thread{
     private int da;
     private final int a;
+    //Prendo i file
+    File encryptedFile = new File("/home/root_over/Documents/Universita/Anno 2/Sistemi operativi/Eserciztazioni/codiceSO/SecondaEsercitazione/document2022.encrypted");
+    File decryptedFile = new File("/home/root_over/Documents/Universita/Anno 2/Sistemi operativi/Eserciztazioni/codiceSO/SecondaEsercitazione/document2022.decrypted");
+
     StringBuilder codice = new StringBuilder();
 
-    public CodeCracker(int da, int a){
+    //FATTO
+    public CodeCracker(int da, int a) throws FileNotFoundException {
         this.da=da;
         this.a=a;
     }
 
+    //prendo i byte dei file
+    private byte[] getByte () throws IOException {
+        FileInputStream inputStream = new FileInputStream(encryptedFile);
+        byte[] inputBytes = new byte[(int) encryptedFile.length()];
+        inputStream.read(inputBytes);
+        return inputBytes;
+    }
+
     @Override //TODO 214748365 > 2147483647
     public void run() {
+        System.out.println("Thread inizia da "+da+" a "+a);
         while (da<=a) {
             int len = 0;
             len = 16 - String.valueOf(da).length();
@@ -36,7 +44,7 @@ public class CodeCracker extends Thread{
             codice.append(da);
             da+=1;
             try {
-                decripta(codice.toString());
+                decripta(codice.toString(),getByte());
                 codice= new StringBuilder("");
             } catch (Exception e) {
                 e.printStackTrace();
@@ -45,20 +53,27 @@ public class CodeCracker extends Thread{
     }
 
 
-    private void decripta(String codice) throws Exception {
+    private void decripta(String codice, byte[]input) throws Exception {
         String key = codice;
-        File inputFile = new File("document.txt");
-        File encryptedFile = new File("/home/root_over/Documents/Universita/Anno 2/Sistemi operativi/Eserciztazioni/codiceSO/SecondaEsercitazione/document2022.encrypted");
-        File decryptedFile = new File("/home/root_over/Documents/Universita/Anno 2/Sistemi operativi/Eserciztazioni/codiceSO/SecondaEsercitazione/document2022.decrypted");
         try{
-            CryptoUtils.decrypt(key,encryptedFile,decryptedFile);
-        }catch (CryptoException ex){
-            System.out.println(ex.getMessage());
-            ex.printStackTrace();
+            Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            System.out.println("PROVO LA CHIAVE "+key);
+            byte[] outputBytes = cipher.doFinal(input);
+            System.out.println("EDDAJE "+key);
+            System.out.println("LA CHIAVE E': "+key);
+            System.exit(-1);
+            FileOutputStream outputStream = new FileOutputStream(decryptedFile);
+            outputStream.write(outputBytes);
+        }catch(NoSuchPaddingException | NoSuchAlgorithmException
+                | InvalidKeyException | BadPaddingException
+                | IllegalBlockSizeException | IOException ex){
         }
     }
-
-    public static void main(String[] args) {
+    //TODO RICORDA DI CHIUDERE I FILE QUANDO TROVA IL CODICE
+//FATTO
+    public static void main(String[] args) throws FileNotFoundException {
         int M=10;
         int da=0;
         int a=214748364;
